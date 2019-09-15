9f60548327ed23a01a0ee5a17e674c261534332d
hojinshim <hojinshim@mz.co.kr>
2019-09-15 14:50:25 +0900

    Add missing worker node's EC2 tag.

--- terraform/eks/modules/eks/eks_worker_nodes.tf
+++ terraform/eks/modules/eks/eks_worker_nodes.tf
@@ -19,48 +19,48 @@ resource "aws_security_group" "eks_worker_nodes_sg" {
 }
 
 resource "aws_security_group_rule" "eks_workder_node_ingress_tcp1" {
-  source_security_group_id      = aws_security_group.eks_cluster_sg.id
-  description                   = "Allow 1025 to 65535 from EKS Control Plane"
-  from_port                     = 1025
-  to_port                       = 65535
-  protocol                      = "tcp"
-  security_group_id             = aws_security_group.eks_worker_nodes_sg.id
-  type                          = "ingress"
+  source_security_group_id = aws_security_group.eks_cluster_sg.id
+  description = "Allow 1025 to 65535 from EKS Control Plane"
+  from_port = 1025
+  to_port = 65535
+  protocol = "tcp"
+  security_group_id = aws_security_group.eks_worker_nodes_sg.id
+  type = "ingress"
 }
 
 resource "aws_security_group_rule" "eks_workder_node_ingress_tcp2" {
-  source_security_group_id      = aws_security_group.eks_cluster_sg.id
-  description                   = "Allow 443 from EKS Control Plane"
-  from_port                     = 443
-  to_port                       = 443
-  protocol                      = "tcp"
-  security_group_id             = aws_security_group.eks_worker_nodes_sg.id
-  type                          = "ingress"
+  source_security_group_id = aws_security_group.eks_cluster_sg.id
+  description = "Allow 443 from EKS Control Plane"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.eks_worker_nodes_sg.id
+  type = "ingress"
 }
 
 resource "aws_security_group_rule" "eks_worker_node_kubectl_access" {
-  source_security_group_id      = aws_security_group.kubectl_sg.id
-  description                   = "Allow SSH traffic from kubectl"
-  from_port                     = 22
-  to_port                       = 22
-  protocol                      = "tcp"
-  security_group_id             = aws_security_group.eks_worker_nodes_sg.id
-  type                          = "ingress"
+  source_security_group_id = aws_security_group.kubectl_sg.id
+  description = "Allow SSH traffic from kubectl"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  security_group_id = aws_security_group.eks_worker_nodes_sg.id
+  type = "ingress"
 }
 
 resource "aws_security_group_rule" "eks_workder_node_ingress_between_nodes" {
-  source_security_group_id      = aws_security_group.eks_worker_nodes_sg.id
-  description                   = "Allow all traffic from worker nodes each other"
-  from_port                     = 0
-  to_port                       = 65535
-  protocol                      = "tcp"
-  security_group_id             = aws_security_group.eks_worker_nodes_sg.id
-  type                          = "ingress"
+  source_security_group_id = aws_security_group.eks_worker_nodes_sg.id
+  description = "Allow all traffic from worker nodes each other"
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  security_group_id = aws_security_group.eks_worker_nodes_sg.id
+  type = "ingress"
 }
 
 resource "aws_iam_role" "eks_worker_node_role" {
-  name                = var.eks_worker_node_role_name
-  assume_role_policy  = file("${path.module}/templates/common_ec2_assume_role_policy.json")
+  name = var.eks_worker_node_role_name
+  assume_role_policy = file("${path.module}/templates/common_ec2_assume_role_policy.json")
 }
 
 resource "aws_iam_instance_profile" "eks_app_worker_node_profile" {
@@ -69,18 +69,18 @@ resource "aws_iam_instance_profile" "eks_app_worker_node_profile" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_app_worker_role_AmazonEKSWorkerNodePolicy" {
-  policy_arn          =   "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role                =   aws_iam_role.eks_worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.eks_worker_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_app_worker_role_AmazonEKS_CNI_Policy" {
-  policy_arn          =   "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role                =   aws_iam_role.eks_worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.eks_worker_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_app_worker_role_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn          =   "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role                =   aws_iam_role.eks_worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.eks_worker_node_role.name
 }
 
 resource "aws_launch_configuration" "eks_worker_nodes_lc" {
@@ -89,12 +89,12 @@ resource "aws_launch_configuration" "eks_worker_nodes_lc" {
   security_groups = [
     aws_security_group.eks_worker_nodes_sg.id
   ]
-  iam_instance_profile          = aws_iam_instance_profile.eks_app_worker_node_profile.name
-  image_id                      = var.eks_worker_node_ami_id
-  instance_type                 = var.eks_worker_node_ec2_type
-  key_name                      = var.kubectl_ec2_keypair
-  user_data                     = data.template_file.eks_worker_node_userdata.rendered
-  enable_monitoring             = var.eks_worker_nodes_enable_monitoring
+  iam_instance_profile = aws_iam_instance_profile.eks_app_worker_node_profile.name
+  image_id = var.eks_worker_node_ami_id
+  instance_type = var.eks_worker_node_ec2_type
+  key_name = var.kubectl_ec2_keypair
+  user_data = data.template_file.eks_worker_node_userdata.rendered
+  enable_monitoring = var.eks_worker_nodes_enable_monitoring
 
   lifecycle {
     create_before_destroy = true
@@ -128,6 +128,12 @@ resource "aws_autoscaling_group" "eks_worker_nodes_asg" {
   tag {
     key = "Managed_by"
     value = "terraform"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key = "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}"
+    value = "owned"
     propagate_at_launch = true
   }
 
